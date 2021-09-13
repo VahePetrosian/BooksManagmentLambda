167e9081bbdb7160813b24c00e87501e38d88aef
VahePetrosian <pvage@mail.ru>
2021-09-13 14:14:12 +0300

    added logic to create file in s3 bucket

--- BooksManagmentLambda/ChangeLogLambda/Function.cs
+++ BooksManagmentLambda/ChangeLogLambda/Function.cs
@@ -1,51 +1,45 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
 
-
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace ChangeLogLambda
 {
-    public class Function
+  public class Function
+  {
+    public Function()
     {
-        /// <summary>
-        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
-        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
-        /// region the Lambda function is executed in.
-        /// </summary>
-        public Function()
-        {
 
-        }
-
-
-        /// <summary>
-        /// This method is called for every Lambda invocation. This method takes in an SQS event object and can be used 
-        /// to respond to SQS messages.
-        /// </summary>
-        /// <param name="evnt"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
-        {
-            foreach(var message in evnt.Records)
-            {
-                await ProcessMessageAsync(message, context);
-            }
-        }
-
-        private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
-        {
-            context.Logger.LogLine($"Processed message {message.Body}");
-
-            // TODO: Do interesting work based on the new message
-            await Task.CompletedTask;
-        }
     }
+
+    public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
+    {
+      foreach (var message in evnt.Records)
+      {
+        await ProcessMessageAsync(message, context);
+      }
+    }
+
+    private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+    {
+      var client = new AmazonS3Client(RegionEndpoint.EUCentral1);
+
+      var request = new PutObjectRequest
+      {
+        BucketName = "log-bucket-books-v-luigfsddasf",
+        Key = $"{DateTime.Now}-log.txt",
+        ContentBody = message.Body,
+        ContentType = "text/plain",
+        StorageClass = S3StorageClass.Standard,
+        CannedACL = S3CannedACL.NoACL
+      };
+
+      await client.PutObjectAsync(request);
+    }
+  }
 }
